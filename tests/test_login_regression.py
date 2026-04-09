519efc8613ed1a74d872c74c147603f060b894f4
Samsharon05 <y.samsharon@gmail.com>
2026-04-10 03:34:06 +0530

    refactor: improved test naming for better readability

--- tests/test_login_regression.py
+++ tests/test_login_regression.py
@@ -9,11 +9,12 @@ data = Configdata()
 class TestLoginRegression:
     
     @pytest.mark.regression
-    def test_login_regression(self, driver):
+    def test_login_successful_user(self, driver): 
         login_page = LoginPage(driver)
         home_page = HomePage(driver) 
         login_page.login(data.login_username, data.login_password)
         assert home_page.is_inventory_visible() 
+        
     @pytest.mark.regression
     def test_login_with_invalid_credentials(self, driver):
         login_page = LoginPage(driver)
